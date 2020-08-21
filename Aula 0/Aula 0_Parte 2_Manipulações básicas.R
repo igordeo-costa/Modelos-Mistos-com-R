02a8e74694a953f4597fc52a8baa6827e8241dbf
igordeo-costa <69773445+igordeo-costa@users.noreply.github.com>
2020-08-21 15:56:14 -0300

    Update Aula 0_Parte 2_Manipulações básicas.R

--- Aula 0/Aula 0_Parte 2_Manipulações básicas.R
+++ Aula 0/Aula 0_Parte 2_Manipulações básicas.R
@@ -1,5 +1,15 @@
-# Parte 2: Importando arquivos .csv para o R
-# (B) manipulações básicas de data frames
+# Aula 0 - Parte 2: Importando arquivos .csv para o R
+
+# O que é um arquivo .csv?
+# O R permite a importação de planilhas direto do excel, mas essa é uma funcionalidade pouco usada.
+# Por isso, não falaremos dela aqui.
+
+# O melhor caminho para importar dados para o R é, ANTES de importá-los, convertê-los para formato .csv
+# (comma separated values). Basicamente, um arquivo .csv é uma planilha, mas em formato de texto, o que
+# a deixa muito leve (mas praticamente impossível de ler a olho nu). Por isso, é preciso um programa
+# (como o R), para ler os dados.
+
+# (A) manipulações básicas de data frames
 
 # Importando uma planilha em formato .csv para o R
 # A função read.csv() permite que se coloque um "caminho" e se busque um arquivo em uma pasta.
@@ -9,7 +19,8 @@
 
 getwd()
 
-#[1] "C:/Users/Igor/Documents"
+# No meu caso, esse é o diretório:
+# [1] "C:/Users/Igor/Documents"
 
 # Os documentos (planilhas, por exemplo) que você usar têm de estar salvos dentro desse diretório.
 
@@ -17,69 +28,78 @@ getwd()
 
 # read.csv()
 
-#Não se esqueça de atribuir um "nome" para seu arquivo, no caso "x":
-# Perceba que "Pasta1.csv" é o nome do arquivo na pasta Documents.
-# No seu computador será diferente.
+# Não se esqueça de atribuir um "nome" para seu objeto, no caso "x":
 
-x=read.csv2("C:/Users/Igor/Documents/Pasta1.csv")
+x=read.csv2("C:/Users/Igor/Documents/minha_tabela.csv") # o objeto "x" recebe o arquivo chamado "minha_tabela.csv".
 
-# Isso não vai funcionar para você, porque você não tem a "Pasta1.csv"
-# E o seu caminho é diferente. Mas não se desespere.
-# Pule esse parte e venha aqui para baixo:
+# Perceba que "minha_tabela.csv" é o nome do arquivo na pasta Documents.
+# No seu computador será diferente, por isso, o comando acima não funcinou para você:
+# Mas não se desespere. Pule esse parte e venha aqui para baixo:
 
 # Primeiro, vamos "setar" um novo diretório de trabalho.
-# Minimize o R, vá até sua "Área de Trabalho" (Windows) ou "Mesa" (Mac) e crie uma nova pasta.
+# Minimize o R, vá até uma pasta de sua escolha (no meu caso: Igor>Documents) e crie uma nova pasta.
 # Dê o nome de "Curso de R" para essa nova pasta.
 
-# Segundo, reabra o R Studio:
+# Em seguida, use o seguinte código, definindo o caminho até sua pasta:
+
+setwd("C:/Users/Igor/Documents/Curso de R")
+
+# Confira se de fato foi setado com
+
+getwd()
+
+#---------------
+# Você pode fazer isso manualmente também:
+
 # No painel à direita, abaixo, clique em "Files".
 # Ache sua "Área de trabalho" ou "Mesa" aí.
 # Se você não achou, vá nos 3 pontinhos (...) bem à direita e clique ali.
 # Daí clique na sua área de trabalho.
 
 # Terceiro, marque a pasta que acabmos de criar "Curso de R".
-# Vá em "More" (a engrenagem azul)
-# E clique em "Set as working directory"
+# Vá em "More" (a engrenagem azul) e clique em "Set as working directory".
+#---------------
 
 # A partir de agora, tudo o que você fizer no R estará vinculado a essa pasta.
 
-# Baixe a pasta do curso da dropbox e coloque dentro dessa pasta.
+# Baixe a pasta do curso do diretório do git e coloque dentro dessa pasta.
 # Agora vamos começar a trabalhar no que realmente interessa.
 
 #---------------------------------------------------------------------------------------------
 # Vamos importar os dados incorporando à função read.csv() a função file.choose():
 
-x=read.csv(file.choose())
+#-----
+# x=read.csv(file.choose())
+#-----
 
-#Nesse caso, a função file.choose() abre uma janela para que você busque o arquivo no diretório
+# Nesse caso, a função file.choose() abre uma janela para que você busque o arquivo no diretório
 # adequado. 
 # Vamos carregar o arquivo chamado "Populações Fictícias.csv" e investigar suas propriedades.
-# Esse arquivo está dentro da pasta Exercícios.
+# Esse arquivo está dentro da pasta Aula 0.
 
 x=read.csv(file.choose())
 
-#Mostrar as 6 primeiras linhas do objeto "x":
+# Mostrar as 6 primeiras linhas do objeto "x":
 
 head(x)
 
-#Mostrar as 20 primeiras linhas do objeto "x":
+# Mostrar as 20 primeiras linhas do objeto "x":
 
 head(x, 20)
 
-
-#Mostrar as 6 últimas linhas do objeto "x":
+# Mostrar as 6 últimas linhas do objeto "x":
 
 tail(x)
 
-#Mostrar as 15 últimas linhas do objeto "x":
+# Mostrar as 15 últimas linhas do objeto "x":
 
 tail(x, 15)
 
-#Dizer o número total de linhas do objeto "x":
+# Dizer o número total de linhas do objeto "x":
 
 nrow(x)
 
-#Dizer o número de colunas do objeto "x":
+# Dizer o número de colunas do objeto "x":
 
 ncol(x)
 
@@ -94,10 +114,10 @@ str(x)
 # A variável "X" é do tipo "int" ("integer", ou seja, números inteiros);
 # As variáveis "popA" e "popB" são do tipo "num" ("numeric", ou seja, números inteiros ou não).
 
-#OBS: Como já visto anteriormente, o R trabalha com diversos tipos de variáveis:
+# OBS: Como já visto anteriormente, o R trabalha com diversos tipos de variáveis:
 # character; numeric; integer; logical; complex; etc.
 
-# Apêndice!
+#---------
 # Manipulando bem pouquinho seu data frame:
 
 # Observe que o objeto "x" tem 3 colunas (reimprima sua "cabeça" na tela):
@@ -106,19 +126,19 @@ str(x)
 x=edit(x) # Estou criando um novo objeto x (que vai se sobrepôr ao anterior),
 # e que será composto pela edição de x (o original).
 # Com essa função, você pode mudar manualmente algo na sua tabela:
-# mude os nomes das colunas para "individuos", "A" e "B", respectivamente.
+# mude os nomes das colunas para "indivíduos", "A" e "B", respectivamente.
 
 #-------------------------------------------------
 # Não faça isso:
 
-edit(x) # ou melhor, faça e veja o que acontece!
+edit(x) # ou melhor, faça e veja o que acontece! Fique tranquilo, seu computador não vai explodir.
 #-------------------------------------------------
 
 # Veja a cabeça da sua tabela:
 
 head(x)
 
-# Vamos mudar os nomes das minhas colunas
+# Vamos mudar os nomes das nossas colunas
 
 # Vamos mudar mais uma vez os nomes das colunas, voltando ao que estava antes,
 # mas agora vamos fazer com a função colnames():
